56d858cb94ff37255c3a9e87d8fa4b5e0b565a9e
ptitbob <github@shipstone.org>
2017-08-02 23:44:13 +0200

    Configuration de gulp

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+const gulp = require('gulp');
+const ts = require('gulp-typescript');
+const JSON_FILES = ['src/*.json', 'src/**/*.json'];
+
+// Intégrer la configuration typescript au projet
+const tsProject = ts.createProject('tsconfig.json');
+
+// Tache de transpilation ts vers js
+gulp.task('typescript', () => {
+    const tsResult = tsProject.src()
+        .pipe(tsProject());
+    return tsResult.js.pipe(gulp.dest('dist'));
+})
+
+// Traitement des fichiers JSON
+gulp.task('json-files', () => {
+    return gulp.src(JSON_FILES)
+        .pipe(gulp.dest("dist"));
+})
+
+// Tache de surveillance des sources et au cas ou compilation de celle-ci.
+gulp.task('watch', () => {
+    gulp.watch("src/**/*.ts", ['typescript']);
+    gulp.watch(JSON_FILES, ['json-files'])
+})
+
+gulp.task('default', ['typescript', 'json-files']);
